705bd6033c1756d70ff6fc6df7586698b76e691c
Erich Zimmerman <erichjz@gmail.com>
2025-02-18 19:49:20 +0000

    Beginning project with driver

---
+++ learning-jdbc/src/main/java/com/erich/lil/data/util/DatabaseUtils.java
@@ -0,0 +1,34 @@
+package com.erich.lil.data.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class DatabaseUtils {
+  private static final String URL = "jdbc:postgresql://localhost:5432/localdb";
+  private static final String USERNAME = "localdbuser";
+  private static final String PASSWORD = "P@ssw0rd!";
+  private static final Logger LOGGER = Logger.getLogger(DatabaseUtils.class.getName());
+  private static final String exceptionFormat = "exception in %s, message: %s, code: %s";
+  private static Connection connection;
+
+  public static Connection getConnection() {
+    if (connection == null) {
+      sychronized(DatabaseUtils.class) {
+        if (connection == null) {
+          try {
+            connection = DriverManager.getConection(URL, USERNAME, PASSWORD);
+          } catch (SQLException e) {
+            handleSqlException("DatabaseUtils.getConnection", e, LOGGER);
+          }
+        }
+      }
+    }
+    return connection;
+  }
+
+  public static void handleSqlException(String method, SqlException e, Logger log) {
+    log.warning(String.format(exceptionFormat, method, e.getMessage(), e.getErrorCode()));
+    throw new RuntimeException(e);
+  }
+}
